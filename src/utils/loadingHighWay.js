4b84c1dd99f503142829237932ad3cae878237d7
N-DK <nguyenkhoadang135759@gmail.com>
2025-02-14 18:54:14 +0700

    only highways

--- src/utils/loadingHighWay.js
+++ src/utils/loadingHighWay.js
@@ -6,7 +6,7 @@ function initData() {
     console.time('Loading data');
 
     const highways = createPromise('highways');
-    const trunks = createPromise('trunks');
+    // const trunks = createPromise('trunks');
 
     let netKeys = {};
     let data = {};
@@ -40,7 +40,7 @@ function initData() {
     };
 
     processItems(highways);
-    processItems(trunks);
+    // processItems(trunks);
 
     console.timeEnd('Loading data');
 
